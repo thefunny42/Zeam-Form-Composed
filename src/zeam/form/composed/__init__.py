4a547ba69655812cb4665c202f196dc391246c22
Sylvain Viollon <thefunny@gmail.com>
2009-10-13 13:20:57 +0200

    Import Grok directives for a direct use.

--- src/zeam/form/composed/__init__.py
+++ src/zeam/form/composed/__init__.py
@@ -1,3 +1,6 @@
 
 from zeam.form.composed.form import ComposedForm, SubForm
 
+from grokcore.component import context
+from grokcore.view import layer, name, require
+from grokcore.viewlet import view, order
